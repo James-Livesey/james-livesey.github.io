ef7a965760df17c474944cf1338f054729376c02
James-Livesey <jamesliveg@gmail.com>
2018-12-01 11:38:52 +0000

    Make website good

---
+++ 404.js
@@ -0,0 +1,28 @@
+$(function() {
+    var lines = [
+        "Beginning dump of physical memory",
+        "",
+        "Physical memory dump complete.",
+        "Contact your system administrator or technical support group for further<br>assistance.",
+        "",
+        "Now taking you to the homepage in 5 seconds",
+        "",
+        "",
+        "",
+        "",
+        "Get ready..."
+    ];
+    var currentLine = 0;
+
+    setInterval(function() {
+        if (currentLine < lines.length) {
+            if (lines[currentLine] != "") {
+                $(".errorStatus").html($(".errorStatus").html() + lines[currentLine] + "<br>");
+            }
+
+            currentLine++;
+        } else {
+            window.location.href = "/index.html";
+        }
+    }, 1000);
+});
